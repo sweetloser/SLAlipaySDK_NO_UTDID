5ccac0cd9a4e6ecaf201f1541846b1e9cbb56336
sweetloser <18272160172@163.com>
2025-08-14 15:48:08 +0800

    feat[#task0]alipaysdk: 版本升级到15.8.35.6
    [Desc]
    None
    [Solution]
    None
    [Impact(None/Fast/Stable/Space/Function/Security/Privacy)]
    None
    [Self-Test Result(Pass/Fail)]
    Pass
    [Test Suggestions]
    None

--- Example/Pods/Target Support Files/Pods-SLAlipaySDK_NO_UTDID_Example/Pods-SLAlipaySDK_NO_UTDID_Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-SLAlipaySDK_NO_UTDID_Example/Pods-SLAlipaySDK_NO_UTDID_Example-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/../../SLAlipaySDK_NO_UTDID/AlipaySDK-15.8.11.15/AlipaySDK.bundle"
+  install_resource "${PODS_ROOT}/../../SLAlipaySDK_NO_UTDID/AlipaySDK-15.8.35.6/AlipaySDK.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/../../SLAlipaySDK_NO_UTDID/AlipaySDK-15.8.11.15/AlipaySDK.bundle"
+  install_resource "${PODS_ROOT}/../../SLAlipaySDK_NO_UTDID/AlipaySDK-15.8.35.6/AlipaySDK.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
